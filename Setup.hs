58f80c3eeedd5559abefb9746948a37f0816242f
Arjun Comar <nrujac@gmail.com>
2013-09-15 00:51:16 -0400

    Some efforts to get cabal to build the package...

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,16 @@
 import Distribution.Simple
-main = defaultMain
+import Distribution.Simple.Setup
+import Distribution.Simple.Utils (rawSystemExit)
+import Distribution.PackageDescription
+
+main :: IO ()
+main = defaultMainWithHooks simpleUserHooks { preConf = \a b -> generateCPP a b >> preConf simpleUserHooks a b 
+                                            , postClean = \a b c d -> cleanCPP a b c >> postClean simpleUserHooks a b c d }
+                        
+generateCPP :: Args -> ConfigFlags -> IO ()
+generateCPP _ flags = rawSystemExit (fromFlag $ configVerbosity flags) "env"
+                        ["python", "cbits/genc.py", "cbits/"]
+
+cleanCPP :: Args -> CleanFlags -> PackageDescription -> IO () 
+cleanCPP _ flags _ = rawSystemExit (fromFlag $ cleanVerbosity flags) "env"
+                          ["rm", "-f", "cbits/opencv_generated.cpp", "cbits/opencv_generated.hpp"]
