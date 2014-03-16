93c08a472adbb831ae2c247402372115b7492026
Arjun Comar <nrujac@gmail.com>
2014-03-15 21:22:42 -0400

    Generalized the relatively useless indexP pipe -- mostly just playing around.

--- Revelation/Main.hs
+++ Revelation/Main.hs
@@ -2,13 +2,13 @@
 module Main where
 import Revelation
 import Linear
-import Control.Monad
+import Data.Word
 
 main :: IO ()
 main = runCV . runEffect $ 
        (cameraCapture 0 :: VideoCapture RGB e)
        >-> convertColorP
-       >-> indexP (V2 0 0)
+       >-> (indexP (V2 0 0) :: Pipe (Mat Grayscale Word8) (Mat Grayscale Word8) CV ())
        >-> waitKeyP (Just 'q') 10 
        >-> (imageDisplayWindow "Test" :: Window Grayscale e)
 
--- Revelation/Utils.hs
+++ Revelation/Utils.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleContexts #-}
 module Revelation.Utils where
 import Pipes
 import Linear
@@ -5,11 +6,11 @@ import Revelation.Mat
 import Revelation.Core
 import Control.Monad
 import Control.Lens
+import Data.Vector.Storable as VS
 
-indexP :: V2 Int -> Pipe (Mat Grayscale Int) (Mat Grayscale Int) CV ()
+indexP :: (Storable (ElemT c e), Show (ElemT c e)) => V2 Int -> Pipe (Mat c e) (Mat c e) CV ()
 indexP i = forever $ do 
               m <- await
-              m' <- lift $ return m & pixel i .~ return (V1 0)
-              v <- lift $ return m' ^. pixel i
+              v <- lift $ return m ^. pixel i
               liftCV $ print (show v)
               yield m
