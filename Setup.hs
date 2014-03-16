4c1ba3da9f5eea5bc1f06139e46069ab9de6bd65
Arjun Comar <nrujac@gmail.com>
2014-03-15 20:59:17 -0400

    Removed unnecessary post-configure hook.

--- Setup.hs
+++ Setup.hs
@@ -1,8 +1,4 @@
 import Distribution.Simple
 
 main :: IO ()
-main = defaultMainWithHooks simpleUserHooks {
-                                            -- The following hook prevents cabal from checking to see if generated cpp header and
-                                            -- source files actually compile before completing configuration.
-                                            postConf = postConf emptyUserHooks } 
-
+main = defaultMainWithHooks simpleUserHooks
